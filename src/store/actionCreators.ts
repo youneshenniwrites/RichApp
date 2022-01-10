dd6fe6bdda41429698b56e42e4b393c84cebc4ab
Younes Henni <henni1989younes@gmail.com>
2022-01-10 14:03:38 +0000

    add action creator with redux dispatch type

---
+++ src/store/actionCreators.ts
@@ -0,0 +1,33 @@
+import { Dispatch } from 'redux';
+import axios from 'axios';
+
+import { Action } from './actions';
+import { ActionType } from './actionTypes';
+
+const BASE_URL = 'https://registry.npmjs.org/-/v1/search';
+
+const searchLibraries = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.SEARCH,
+    });
+
+    try {
+      const { data } = await axios.get(BASE_URL, {
+        params: { term },
+      });
+
+      const names = data.objects.map((result: any) => result.package.name);
+
+      dispatch({
+        type: ActionType.SEARCH_SUCCESS,
+        payload: names,
+      });
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.SEARCH_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
