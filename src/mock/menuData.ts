6fdec0739d206c9929e7040ec29e8e3031276197
Younes Henni <henni1989younes@gmail.com>
2022-01-12 19:34:52 +0000

    add urls for food images in mock api

--- src/mock/menuData.ts
+++ src/mock/menuData.ts
@@ -4,7 +4,8 @@ export const menuData = [
     name: 'Napoletana',
     type: 'Pizza',
     description: 'Fresh tomatoes and mozzarella cheese.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2020/06/08/16/49/pizza-5275191_1280.jpg',
     price: 7.99,
   },
   {
@@ -12,7 +13,8 @@ export const menuData = [
     name: 'Beef and Cheese',
     type: 'Burger',
     description: 'Beaf, salad, tomatoes, and cheddar cheese.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_1280.jpg',
     price: 9.99,
   },
   {
@@ -20,7 +22,8 @@ export const menuData = [
     name: 'Marinara',
     type: 'Pizza',
     description: 'Fresh tomatoes, extra virgin olive oil, oregano and garlic.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2017/01/03/11/33/pizza-1949183_1280.jpg',
     price: 7.99,
   },
   {
@@ -28,7 +31,8 @@ export const menuData = [
     name: 'MacChicken',
     type: 'Burger',
     description: 'Chicken, tomatoes, salad, and cheddar cheese.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2018/03/04/20/08/burger-3199088_1280.jpg',
     price: 9.99,
   },
   {
@@ -36,23 +40,26 @@ export const menuData = [
     name: 'Lentil Soup',
     type: 'Soup',
     description: 'Lentil, carrots, tomatoes, and garlic.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2017/03/17/17/33/potato-soup-2152254_1280.jpg',
     price: 11.99,
   },
   {
     id: 6,
-    name: 'Ham Sandwich',
+    name: 'Mexican Sandwich',
     type: 'Sandwich',
-    description: 'Ham, fresh tomatoes, and mozzarella cheese.',
-    picture: 'https://picsum.photos/700',
-    price: 6.99,
+    description: 'Fried chicken, jalapeno, tomatoes, and mozzarella cheese.',
+    picture:
+      'https://cdn.pixabay.com/photo/2016/11/29/04/00/bread-1867208_1280.jpg',
+    price: 7.99,
   },
   {
     id: 7,
     name: 'Caprese',
     type: 'Pizza',
     description: 'Fresh mozzarella, tomatoes, basil and balsamic vinegar.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2017/09/30/15/10/plate-2802332_1280.jpg',
     price: 11.99,
   },
   {
@@ -60,15 +67,17 @@ export const menuData = [
     name: 'Chicken Soup',
     type: 'Soup',
     description: 'Chicken, fresh tomatoes, carrots, and breadcrumbs.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2017/01/24/20/30/soup-2006317_1280.jpg',
     price: 9.99,
   },
   {
     id: 9,
     name: 'Kebab Mix',
     type: 'Sandwich',
-    description: 'Chicken, lamb, tomatoes, and chips.',
-    picture: 'https://picsum.photos/700',
+    description: 'Chicken, lamb, tomatoes, salad, and chips.',
+    picture:
+      'https://cdn.pixabay.com/photo/2016/10/19/18/25/doner-kebab-1753615_1280.jpg',
     price: 12.99,
   },
   {
@@ -77,7 +86,8 @@ export const menuData = [
     type: 'Pizza',
     description:
       'Fresh tomatoes, mozzarella cheese, basil, salt and olive oil.',
-    picture: 'https://picsum.photos/700',
+    picture:
+      'https://cdn.pixabay.com/photo/2016/11/29/13/02/cheese-1869708_1280.jpg',
     price: 11.99,
   },
 ];
